b67f2840a5ea2c521192b4efba21c45b95433010
kmin <iamlkm0810@gmail.com>
2024-05-17 20:31:40 +0900

    command argc > 2 error

--- client/commands.cpp
+++ client/commands.cpp
@@ -12,7 +12,8 @@ Commands get_command(std::string cmd)
     if (cmd == "h") return HELP;
 
     std::vector<std::string> tokens = split(cmd, " ");
-
+    if(tokens.size() > 2) return INVALID;
+    
     if (tokens[0] == "join") return JOIN;
     if (tokens[0] == "leave") return LEAVE;
     if (tokens[0] == "ls")
