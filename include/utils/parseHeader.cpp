cb1f8522bea9b7c1b29ed1df5331139cfe57aff5
kmin <iamlkm0810@gmail.com>
2024-05-24 00:02:55 +0900

    Enhance utils/parseHeaders: Add message body lines parsing functionality
    - Updated utils/parseHeaders to include parsing functionality for message body lines

--- include/utils/parseHeader.cpp
+++ include/utils/parseHeader.cpp
@@ -4,7 +4,22 @@ std::map<std::string, std::string> parseHeaders(const char* msg)
 {
     std::map<std::string, std::string> headers;
     const char* delimiter = "\r\n";
+    const char* delim_double = "\r\n\r\n";
     char* msg_copy = strdup(msg);
+
+    // Find the position of the double CRLF which separates headers from body
+    char* body_start = strstr(msg_copy, delim_double);
+    
+    if (body_start != nullptr)
+    {
+        // Separate the headers part and the body part
+        *body_start = '\0';
+        body_start += strlen(delim_double);
+        
+        // Insert the body part into the map with a special key
+        headers["body"] = std::string(body_start);
+    }
+
     char* line = strtok(msg_copy, delimiter);
 
     while (line != nullptr)
